f9d95f288cdd92c29c5419daae0ee8df70ea5a13
FraserAllanBest <vagrant@fed-dev-box>
2015-08-04 05:57:58 +0000

    added sticky waypoint colored menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 $(document).ready(function(){
 
@@ -16,5 +16,22 @@ $(document).ready(function(){
 
 	$("div.tag:contains('opinion')").addClass("opinion-tag");
 
+	$("div.nav-button:contains('news')").addClass("news-nav-button");
+	$("div.nav-button:contains('opinion')").addClass("opinion-nav-button");
+	$("div.nav-button:contains('arts')").addClass("arts-nav-button");
+	$("div.nav-button:contains('features')").addClass("features-nav-button");
+	$("div.nav-button:contains('columns')").addClass("columns-nav-button");
+
+
+	var sticky = new Waypoint.Sticky({
+		element: $('.bar')[0]
+	});
+
+	var $menu = $(".bar");
+
+	$menu.waypoint(function()	{
+		$(".bar").toggleClass("bar-locked");
+		$(".cover-photo").toggleClass("sticky-offset")
+	});
 
 });
